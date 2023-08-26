14ce2dbd8e36c3ea783c2501f8dca928d08bf77e
Min He Ng <minhe0611@gmail.com>
2023-08-26 13:06:13 +1000

    111

--- src/page/history.js
+++ src/page/history.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './History.css';
+import '../style/History.css';
 
 class History extends React.Component {
   constructor(props) {
--- src/route.js
+++ src/route.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 
 import HomePage from "./page/HomePage";
+import History from "./page/History";
 
 export default function RouteApp () {
     return (
@@ -9,6 +10,8 @@ export default function RouteApp () {
             <Router>
                 <Routes>
                     <Route path="/" element={<HomePage/>} />
+                    <Route path="/history" element={<History/>} />
+
                 </Routes>
             </Router>
         </>
