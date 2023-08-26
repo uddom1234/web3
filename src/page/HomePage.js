808b0777b960b0b922c7718e3374574c498fb976
Trung <trung.vthai@gmail.com>
2023-08-26 13:26:47 +1000

    homepage udpated

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -10,13 +10,14 @@ import "../style/homepage.css";
 
 const HomePage = () => {
     return (
-        <div class="container">
-    <div class="small-container">
+        <div class="flex h-72 space-x-4">
+    <div class="small-container flex-1 bg-gray-300 rounded-lg">
       <h1 className="text-3xl font-bold text-black underline">
         Small Container
         </h1>
+        <h1 className="text-3xl italic underline">testing </h1>
     </div>
-    <div class="big-container">
+    <div className="big-container flex-2 bg-gray-500 relative rounded-lg big-container-height">
       <div class="scrolling-content">
         big container<br></br>
         line 1 <br></br> 
