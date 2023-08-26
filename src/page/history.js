3d931cf447086b0f6bba5c22c16a03cb3627d752
Min He Ng <minhe0611@gmail.com>
2023-08-26 14:19:07 +1000

    changing style

--- src/page/history.js
+++ src/page/history.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import '../style/History.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 class 
 History extends React.Component {
+  //initialize component properties and state
   constructor(props) {
+    //call the parent class constructor and set up this.props
     super(props);
     this.state = {
-      transactions: [
+      transactionHistory: [
         { id: '123123124312432412432', method: 'mint', date: '09/02/2023' },
         { id: '123123123213213123122', method: 'gift', date: '02/02/2013' },
         { id: '123123123213125456676', method: 'mint', date: '09/02/2019' },
@@ -17,36 +18,38 @@ History extends React.Component {
     };
   }
 
+  //describe the component's UI
   render() {
     return (
         <div className="flex flex-col min-h-screen">
         <Header/>
-        <main className="flex-grow">
-        <h1>History Page</h1>
-        <table border="1">
-          <thead>
-            <tr>
-              <th>Transaction</th>
-              <th>Method</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.transactions.map((transaction, index) => {
-              return (
-                <tr key={index}>
-                  <td>{transaction.id}</td>
-                  <td>{transaction.method}</td>
-                  <td>{transaction.date}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <main className="flex-grow px-8 py-4">
+          <div className="bg-gray-800 p-6 mb-8 text-white border-2 border-black shadow-lg">
+            <h1 className="text-center">History Page</h1>
+          </div>
+          <table className="min-w-full border-collapse border border-black shadow-lg">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border border-black">Transaction</th>
+                <th className="py-2 px-4 border border-black">Method</th>
+                <th className="py-2 px-4 border border-black">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.transactionHistory.map((transactionHistory, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border border-black">{transactionHistory.id}</td>
+                    <td className="py-2 px-4 border border-black">{transactionHistory.method}</td>
+                    <td className="py-2 px-4 border border-black">{transactionHistory.date}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </main>
         <Footer/>
       </div>
-      
     );
   }
 }
