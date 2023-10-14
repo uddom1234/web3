f164a6a72591c6e39617084e49a12343169c3ec9
Min He Ng <minhe0611@gmail.com>
2023-10-14 11:55:28 +1100

    error message

--- src/page/Login.js
+++ src/page/Login.js
@@ -15,6 +15,7 @@ const Login = () => {
     const [isMounted, setIsMounted] = React.useState(false);
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
+    const [error, setError] = React.useState('')
 
     // Handler for the email field
     const handleEmailChange = (event) => {
@@ -45,25 +46,34 @@ const Login = () => {
 
 
     const handleLogin = async (e) => {
-        e.preventDefault()
-
-        try {
+      e.preventDefault();
+      setError('');  // Clear the existing error
+  
+      if (!email || !password) {
+          setError("Both fields are required.");
+          return;
+      }
+  
+      try {
           // Post request using axios
-          const response = await axios.post('http://localhost:8000/login', {
-            email: email,
-            password: password,
+          const response = await axios.post('http://localhost:8000/login/', {
+              email: email,
+              password: password,
           });
-
+  
           dispatch(authAction.login());
           navigate('/Browse');
-          
-        } catch (error) {
-          console.error("There was an error!", error);
-        }
-
-
-
-    }
+        
+      } catch (error) {
+          console.error("There was an error logging in!", error);
+          if (error.response && error.response.status === 400) {
+              // Handle Bad Request
+              setError(error.response.data.detail);
+          } else {
+              setError("An unexpected error occurred. Please try again.");
+          }
+      }
+  }
 
     return (
         <div className="flex flex-col min-h-screen">
@@ -77,24 +87,24 @@ const Login = () => {
                     </div>
                         <form className="w-full max-w-sm mx-auto" onSubmit={handleLogin}> {/* Textfield for entering credentials*/}
                         <div className="mb-4">
-        <TextField 
-          fullWidth 
-          label="Email" // Changed to "Email" because we're using setEmail
-          variant="outlined"
-          value={email} // value is controlled by the component's state
-          onChange={handleEmailChange} // set onChange to the handler
-        />
-      </div>
-      <div className="mb-4">
-        <TextField 
-          fullWidth 
-          label="Password" 
-          type="password" 
-          variant="outlined" 
-          value={password} // value is controlled by the component's state
-          onChange={handlePasswordChange} // set onChange to the handler
-        />
-      </div>
+                                  <TextField 
+                                    fullWidth 
+                                    label="Email" // Changed to "Email" because we're using setEmail
+                                    variant="outlined"
+                                    value={email} // value is controlled by the component's state
+                                    onChange={handleEmailChange} // set onChange to the handler
+                                  />
+                                </div>
+                                <div className="mb-4">
+                                  <TextField 
+                                    fullWidth 
+                                    label="Password" 
+                                    type="password" 
+                                    variant="outlined" 
+                                    value={password} // value is controlled by the component's state
+                                    onChange={handlePasswordChange} // set onChange to the handler
+                                  />
+                                </div>
                                 <div className="mb-4">
                                     <Button     
                                         fullWidth 
@@ -110,12 +120,13 @@ const Login = () => {
                                         <Button color="secondary">
                                             Sign Up
                                         </Button>
-                                    </Link>
+                                    </Link> 
                                 </div>
+                                {error && <p className="text-red-500 text-center mb-4">{error}</p>}
                             </form>   
                     </Grid>  
 
-                </Grid>
+                </Grid>``
                 
             </Box>
 
--- src/page/Signup.js
+++ src/page/Signup.js
@@ -6,12 +6,13 @@ import Box from '@mui/material/Box';
 import Footer from "../components/Footer";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
     const [isMounted, setIsMounted] = React.useState(false);
-
+    const [error, setError] = useState()
     //animation for text
     React.useEffect(() => {
         setTimeout(() => {
@@ -22,6 +23,9 @@ const Signup = () => {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [age, setAge] = useState(0); 
+    const [gender, setGender] = useState('');
+
     const navigate = useNavigate();
 
     // Handler for the email field
@@ -39,13 +43,37 @@ const Signup = () => {
       setPassword(event.target.value);
     };
 
+    // Handler for the age field
+    const handleAgeChange = (event) => {
+        setAge(event.target.value);
+    };
+  
+    // Handler for the gender field
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    };
+  
+
     const handleSignup = async (e) => {
         e.preventDefault(); // Prevents the default action of form submission
+        setError('');
 
+        if (password.length < 8) {
+            setError("Password must be at least 8 characters long.");
+            return;
+        }
+
+        if (!email || !username || !password || !age || !gender) {
+            setError("All fields are required.");
+            return;
+        }
+        
         const user = {
             email,
             username,
             password,
+            age,
+            gender, 
         };
 
         try {
@@ -55,8 +83,36 @@ const Signup = () => {
           // Navigate to login page 
           navigate('/login');
         } catch (error) {
-          console.error("There was an error while sending the data!", error);
+            console.error("There was an error while sending the data!", error);
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+        
+                // Parse the error message from the backend response
+                let errorMessage = "An error occurred: Please try again.";
+                if (error.response.status === 400) {
+                    const errorData = error.response.data;
+                    if (errorData.email) {
+                        errorMessage = errorData.email;
+                    } else if (errorData.username) {
+                        errorMessage = errorData.username;
+                    } else {
+                        errorMessage = "Bad request: Please check your input fields.";
+                    }
+                } else if (error.response.status === 500) {
+                    errorMessage = "Internal server error: Please try again later.";
+                }
+                setError(errorMessage); // Set the error message to state
+            } else if (error.request) {
+                console.log(error.request);
+                setError("No response from the server: Please check your internet connection.");
+            } else {
+                console.log('Error', error.message);
+                setError("An error occurred while setting up the request: Please try again.");
+            }
         }
+        
     }
 
     return (
@@ -99,6 +155,31 @@ const Signup = () => {
                                 />
                             </div>
                             <div className="mb-4">
+                            <TextField 
+                                fullWidth 
+                                label="Age" 
+                                variant="outlined"
+                                value={age}
+                                onChange={handleAgeChange}
+                                type="number"  // Ensures only numeric input
+                                InputProps={{ inputProps: { min: 1 } }}  // minimum value
+                            />
+                            </div>
+                            <div className="mb-4">
+                                <TextField 
+                                    fullWidth 
+                                    label="Gender" 
+                                    variant="outlined"
+                                    select   
+                                    value={gender}
+                                    onChange={handleGenderChange} 
+                                >
+                                    {/* Dropdown options */}
+                                    <MenuItem value={"male"}>Male</MenuItem>
+                                    <MenuItem value={"female"}>Female</MenuItem>
+                                </TextField>
+                            </div>
+                            <div className="mb-4">
                                     <Button     
                                         fullWidth 
                                         variant="contained" 
@@ -108,7 +189,10 @@ const Signup = () => {
                                     Connect with BlockHub
                                     </Button>
                                 </div>
-                                    
+                                <div className="flex justify-center items-center">
+                                    <p className="text-red-500 text-lg ">{error}</p>
+                                </div>
+
                                 
                                 <div className="text-center">
                                     <Link to="/login"> {/* Take to log in page if user have an account*/}
